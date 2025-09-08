5bf87d4505cc4dde262adae78fec62990709c007
IMDADUR <imdadurtapadar5@gmail.com>
2025-09-08 19:40:11 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
@@ -10,6 +10,7 @@ const asyncHandler = (requestHandler) => {
  
 
 export {asyncHandler}
+
 
 
 
